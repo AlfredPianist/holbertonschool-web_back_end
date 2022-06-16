57ba4a38554ebc85fc4c128c136d217c6afd90c1
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-16 15:21:03 -0500

    Add: task 6 for 0x05

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -11,3 +11,14 @@ def hash_password(password: str) -> bytes:
         The byte array hashed password.
     """
     return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a hashed password is valid.
+    Args:
+        hashed_password: The hashed password to be checked.
+        password: The original password.
+    Returns:
+        True if valid, False otherwise.
+    """
+    return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
