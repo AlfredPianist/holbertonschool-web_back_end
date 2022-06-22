34b507b7c11da790360b5a76e4d90ba05570b26f
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-21 19:33:00 -0500

    Add task 4 for 0x06"

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -20,7 +20,14 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Function returning if a page requires authentication or not.
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        for excluded_path in excluded_paths:
+            if excluded_path[:-1] == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Function returning the authorization header.
