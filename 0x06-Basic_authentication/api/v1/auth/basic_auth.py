b518b17f3a564dcb2d66162f67f2f5f2b2beba71
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-21 20:39:08 -0500

    Add task 8 for 0x06

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic Auth module
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
                 not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """ Decodes a base64 part of authorization header
+        """
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            base64_authorization_header = base64_authorization_header.encode(
+                'utf-8')
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
