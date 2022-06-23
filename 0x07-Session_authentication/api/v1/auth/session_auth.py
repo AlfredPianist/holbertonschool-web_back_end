7b1ddf33c5ef18d329d6eb1acc96d0e75724a57c
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-23 18:38:58 -0500

    Add tasks 1 and 2 for 0x07

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Session Auth module
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    """ Session Auth class
+    """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates session ID for user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
