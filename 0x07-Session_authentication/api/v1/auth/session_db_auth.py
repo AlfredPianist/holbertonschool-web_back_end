840424e9cbd48a2eed5730528e88ae9fb19d6d41
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-27 22:22:53 -0500

    Fix: task 10 for 0x07

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -16,8 +16,8 @@ class SessionDBAuth(SessionExpAuth):
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
-        user_session = UserSession({"user_id": user_id,
-                                    "session_id": session_id})
+        kwargs = {'user_id': user_id, 'session_id': session_id}
+        user_session = UserSession(**kwargs)
         user_session.save()
         UserSession.save_to_file()
         return session_id
