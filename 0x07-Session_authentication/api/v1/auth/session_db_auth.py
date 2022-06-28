c28c9e36f55edae62510ef26cc08a7c54945e19d
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-27 22:17:27 -0500

    Fix: task 10 for 0x07

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -7,72 +7,47 @@ from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
-    """Session in database Class"""
+    """ Session DB Auth class
+    """
 
     def create_session(self, user_id=None):
-        """Creation session database"""
+        """ Creates session ID for user_id
+        """
         session_id = super().create_session(user_id)
-
         if session_id is None:
             return None
-
-        kwargs = {'user_id': user_id, 'session_id': session_id}
-        user_session = UserSession(**kwargs)
+        user_session = UserSession({"user_id": user_id,
+                                    "session_id": session_id})
         user_session.save()
-        UserSession.save_to_file()
-
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """User ID for Session ID Database"""
+        """ Returns a user_id based on a session_id
+        """
         if session_id is None:
             return None
-
         UserSession.load_from_file()
-        user_session = UserSession.search({
-            'session_id': session_id
-        })
-
+        user_session = UserSession.search({'session_id': session_id})
         if not user_session:
             return None
-
-        user_session = user_session[0]
-
-        expired_time = user_session.created_at + \
+        session_delta = user_session[0].get('created_at') + \
             timedelta(seconds=self.session_duration)
-
-        if expired_time < datetime.utcnow():
+        if session_delta < datetime.now():
             return None
-
-        return user_session.user_id
+        return user_session[0].get(user_id)
 
     def destroy_session(self, request=None):
-        """Remove Session from Database"""
+        """Deletes a User instance (logout)
+        """
         if request is None:
-            return False
-
+            return None
         session_id = self.session_cookie(request)
         if session_id is None:
-            return False
-
-        user_id = self.user_id_for_session_id(session_id)
-
-        if not user_id:
-            return False
-
-        user_session = UserSession.search({
-            'session_id': session_id
-        })
-
-        if not user_session:
-            return False
-
-        user_session = user_session[0]
-
+            return None
         try:
-            user_session.remove()
-            UserSession.save_to_file()
+            user = UserSession.search({"session_id": session_id})
         except Exception:
-            return False
-
-        return True
+            user = []
+        if len(user) == 0:
+            return None
+        user[0].remove()
