84076ddc62a8d1d18b88faa5c21d09f527e35603
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-30 21:28:52 -0500

    Fix: task 0 for 0x08

--- 0x08-user_authentication_service/models/user.py
+++ 0x08-user_authentication_service/models/user.py
@@ -10,13 +10,9 @@ Base = declarative_base()
 class User(Base):
     """ User class
     """
-
-    def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize a User instance
-        """
-        __tablename__ = 'users'
-        id = Column(Integer, primary_key=True)
-        email = Column(String, nullable=False)
-        hashed_password = Column(String, nullable=False)
-        session_id = Column(String)
-        reset_token = Column(String)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String)
+    reset_token = Column(String)
