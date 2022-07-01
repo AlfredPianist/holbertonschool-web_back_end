519fd884df7fa9ce7c7b932970158d5e4ee75a64
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-06-30 21:26:58 -0500

    Add: task 0 for 0x08

---
+++ 0x08-user_authentication_service/models/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" User module
+"""
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a User instance
+        """
+        __tablename__ = 'users'
+        id = Column(Integer, primary_key=True)
+        email = Column(String, nullable=False)
+        hashed_password = Column(String, nullable=False)
+        session_id = Column(String)
+        reset_token = Column(String)
