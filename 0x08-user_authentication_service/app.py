8a92ce5b39e4ca2bcf621f300d07a5ed493d2f81
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-07-03 14:12:22 -0500

    Add: task 7 for 0x08

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,8 +2,10 @@
 """
 Auth module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -15,5 +17,17 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+        Register a user
+    """
+    try:
+        user = AUTH.register_user(**request.form)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
