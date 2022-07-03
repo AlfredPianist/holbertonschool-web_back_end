2662864b68b1673dca37209c11c01d5953207381
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-07-02 22:53:20 -0500

    Fix: task 2 from 0x08

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,48 +1,41 @@
 #!/usr/bin/env python3
-
-"""DB module
+"""
+Implement User class in DB
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm import sessionmaker
+from user import Base
+from user import User
+import re
 
-from user import Base, User
-
-
-def has_keys(**kwargs):
-    """
-    If all the keys in the kwargs dictionary are not in the properties list,
-    raise an error."
-    """
-    properties = [
-        'id',
-        'email',
-        'session_id',
-        'reset_token',
-        'hashed_password'
-    ]
-    return all(
-        [True if key in properties else False for key in kwargs.keys()])
+USER_COLUMN_NAMES_SET = ['id', 'email', 'session_id',
+                         'reset_token', 'hashed_password']
 
 
 class DB:
-    """DB class
+    """
+        DB class
+        create model to manage the Database
     """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    def __init__(self):
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+            Constructor
+            create the object BD
+        """
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
+    def _session(self):
+        """
+            Create if not exists a db session
+            Return the session
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -50,19 +43,24 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the DB
-         """
+        """
+            Create an User and insert in Database
+            Return the new User
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs) -> User:
-        """Find a user by parameters
+    def find_user_by(self, **kwargs: dict) -> User:
         """
-        if not has_keys(**kwargs):
-            raise(InvalidRequestError)
+            Return a user with specificed kwargs.
+        """
+        if not all([True
+                    if key in USER_COLUMN_NAMES_SET
+                    else False for key in kwargs.keys()]):
+            raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
-            raise(NoResultFound)
+            raise NoResultFound
         return user
