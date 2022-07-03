3163f9c85f52b6ff9873d18ea3df25a8069ecc75
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-07-02 23:43:32 -0500

    Add task 5 for 0x08

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,8 +3,29 @@
 Auth module
 """
 import bcrypt
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+            Registers a user.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except ValueError:
+            hashed_password = _hash_password(password)
+            created_user = self._db.add_user(email, hashed_password)
+            return created_user
