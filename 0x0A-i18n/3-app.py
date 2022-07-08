006bb6e7cb8d5ee2f10f5a248b11b46dcc9e3257
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-07-07 19:30:51 -0500

    Add: task 3 for 0x0A

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""Basic Flask app w/ Babel extension"""
+
+from flask import Flask, render_template, request, flash
+from flask_babel import Babel
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config():
+    """Configuration class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello_world() -> str:
+    """Renders 3-index template with Hello World"""
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Gets locale from a best match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
