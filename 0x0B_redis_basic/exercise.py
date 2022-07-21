7079d39c4e293ae05be6d1e6b1f5a824a4e12002
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-07-20 21:21:27 -0500

    Add: task 1 for 0x0B

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,8 @@
 """
 import redis
 import uuid
-from typing import Union
+import sys
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -20,3 +21,16 @@ class Cache:
         id = str(uuid.uuid4())
         self._redis.set(id, data)
         return id
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Gets data from Redis cache"""
+        return fn(self._redis.get(key)) if fn else self._redis.get(key)
+
+    def get_str(value: bytes) -> str:
+        """Converts bytes value to string"""
+        return value.decode('utf-8')
+
+    def get_int(value: bytes) -> int:
+        """Converts bytes value to int"""
+        return int.from_bytes(value, sys.byteorder)
