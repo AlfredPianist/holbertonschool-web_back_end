b94fd0fae4ef9ececdcc28b68321e643aa122194
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-08-05 13:46:34 -0500

    Fix: task 6 for 0x0F. Return array

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -6,15 +6,15 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promiseSignUp = signUpUser(firstName, lastName);
   const promiseUpload = uploadPhoto(fileName);
 
-  Promise.allSettled([promiseSignUp, promiseUpload]).then((results) => {
-    results.forEach((result) => {
-      const value =
-        typeof result.value === 'undefined'
-          ? result.reason.toString()
-          : result.value;
-      console.log({ status: result.status, value: value });
-    });
-  });
-
-  return response;
+  return Promise.allSettled([promiseSignUp, promiseUpload])
+    .then((results) => {
+      results.forEach((result) => {
+        const value =
+          typeof result.value === 'undefined'
+            ? result.reason.toString()
+            : result.value;
+        response.push({ status: result.status, value: value });
+      });
+    })
+    .finally(() => response);
 }
