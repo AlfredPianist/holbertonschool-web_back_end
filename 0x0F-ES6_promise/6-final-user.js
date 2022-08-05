bc956b56fbc87c74b710878ae140976652ddc8d3
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-08-05 14:04:32 -0500

    Fix: task 6 for 0x0F. Lint and return

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,19 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const response = [];
   const promiseSignUp = signUpUser(firstName, lastName);
   const promiseUpload = uploadPhoto(fileName);
 
-  return Promise.allSettled([promiseSignUp, promiseUpload])
-    .then((results) => {
-      results.forEach((result) => {
-        const value =
-          typeof result.value === 'undefined'
-            ? result.reason.toString()
-            : result.value;
-        response.push({ status: result.status, value: value });
-      });
-    })
-    .finally(() => response);
+  return Promise.allSettled([promiseSignUp, promiseUpload]).then((results) => {
+    const response = [];
+    results.forEach((result) => {
+      const value = typeof result.value === 'undefined'
+        ? result.reason.toString()
+        : result.value;
+      response.push({ status: result.status, value });
+    });
+    return response;
+  });
 }
