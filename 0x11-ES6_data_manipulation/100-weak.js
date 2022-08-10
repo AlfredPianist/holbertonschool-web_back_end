242a66e31d8ca776c10c64b6b0751541646d551d
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-08-09 23:42:37 -0500

    Add: task 100 for 0x11

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,7 @@
-export default function updateUniqueItems(groceryList) {
-  try {
-    groceryList.forEach((value, key) => {
-      if (value === 1) groceryList.set(key, 100);
-    });
-  } catch (error) {
-    throw Error('Cannot process');
-  }
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const currCount = weakMap.get(endpoint) || 0;
+  if (currCount !== undefined) weakMap.set(endpoint, currCount + 1);
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
 }
