8ccab605ace89864368909e0146bd31438ab8e95
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-08-11 12:22:25 -0500

    Add: tasks 4 and 5 for 0x12

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const { argv } = require('process');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((request, response) => {
+  const { url } = request;
+  const database = argv[2];
+
+  response.setHeader('Content-Type', 'text/plain');
+
+  switch (url) {
+    case '/':
+      response.statusCode = 200;
+      response.end('Hello Holberton School!');
+      break;
+    case '/students':
+      response.write('This is the list of our students\n');
+      countStudents(database)
+        .then((message) => response.end(message))
+        .catch((error) => response.end(error.message));
+      break;
+    default:
+      response.statusCode = 404;
+      response.end();
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
