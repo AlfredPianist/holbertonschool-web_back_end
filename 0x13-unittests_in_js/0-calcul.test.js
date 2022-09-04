d7ca2397140421d28ff3bacbe50348ab6148f694
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-09-04 11:31:55 -0500

    Add: task 0 for 0x13

---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('#calculateNumber()', () => {
+  it('sums two integers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(-10, 2), -8);
+    assert.strictEqual(calculateNumber(-2, 0), -2);
+  });
+  it('rounds numbers on addition', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('returns NaN when number of arguments is incorrect', () => {
+    assert.strictEqual(isNaN(calculateNumber()), true);
+    assert.strictEqual(isNaN(calculateNumber(1)), true);
+  });
+});
