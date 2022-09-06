ddaeb793c08df52cb5fad2b307226a48a85fcb24
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-09-06 16:53:14 -0500

    Add: task 10 for 0x13

--- 0x13-unittests_in_js/10-api/api.js
+++ 0x13-unittests_in_js/10-api/api.js
@@ -1,14 +1,17 @@
 'use strict';
 const express = require('express');
+
 const app = express();
 const port = 7865;
+
+app.use(express.json());
 
 app.get('/', (request, response) =>
   response.end('Welcome to the payment system')
 );
 
-app.get('/cart/:id([0-9]+)', (request, response) =>
-  response.end(`Payment methods for cart ${request.params.id}`)
+app.get('/cart/:cartId([0-9]+)', (request, response) =>
+  response.end(`Payment methods for cart ${request.params.cartId}`)
 );
 
 app.get('/available_payments', (request, response) => {
@@ -22,12 +25,8 @@ app.get('/available_payments', (request, response) => {
 });
 
 app.post('/login', (request, response) => {
-  const username = request.body.useName;
+  const username = request.body.userName;
   response.end(`Welcome ${username}`);
 });
-
-app.get('/cart/:id([0-9]+)', (request, response) =>
-  response.end(`Payment methods for cart ${request.params.id}`)
-);
 
 app.listen(port, () => console.log(`API available on localhost port ${port}`));
--- 0x13-unittests_in_js/10-api/api.test.js
+++ 0x13-unittests_in_js/10-api/api.test.js
@@ -3,6 +3,20 @@ const request = require('request');
 
 describe('integration test', () => {
   describe('GET /', () => {
+    it('has the correct output on GET / endpoint', (done) => {
+      const call = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/:id', () => {
     it('has the correct output on GET /cart/:id endpoint', (done) => {
       const call = {
         url: 'http://localhost:7865/cart/12',
@@ -22,6 +36,68 @@ describe('integration test', () => {
       };
       request(call, (error, response, body) => {
         expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /available_payments', () => {
+    it('has the correct output on GET /available_payments endpoint', (done) => {
+      const call = {
+        url: 'http://localhost:7865/available_payments',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.deep.equal(
+          '{"payment_methods":{"credit_cards":true,"paypal":false}}'
+        );
+        done();
+      });
+    });
+
+    it('has the correct output on GET /available_payments endpoint when parsed', (done) => {
+      const call = {
+        url: 'http://localhost:7865/available_payments',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(JSON.parse(body)).to.deep.equal({
+          payment_methods: {
+            credit_cards: true,
+            paypal: false,
+          },
+        });
+        done();
+      });
+    });
+  });
+
+  describe('POST /login', () => {
+    it('has the correct output on POST /login endpoint with body', (done) => {
+      const call = {
+        url: 'http://localhost:7865/login',
+        method: 'POST',
+        json: {
+          userName: 'Alfredo',
+        },
+      };
+      request(call, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.deep.equal('Welcome Alfredo');
+        done();
+      });
+    });
+
+    it('has the correct output on POST /login endpoint with no body', (done) => {
+      const call = {
+        url: 'http://localhost:7865/login',
+        method: 'POST',
+      };
+      request(call, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.deep.equal('Welcome undefined');
         done();
       });
     });
